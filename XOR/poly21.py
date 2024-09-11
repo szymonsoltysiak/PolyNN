aaad69b9a0ad713f9bdbbff006f72edc31184800
Soltyssss08 <263495@student.pwr.edu.pl>
2024-09-11 20:22:15 +0200

    XOR with x+y and x*y inputs

--- XOR/poly21.py
+++ XOR/poly21.py
@@ -1,8 +1,5 @@
-#  x  ‾‾‾‾‾‾‾‾‾‾‾‾‾\
-#                   \
-#  y  ------------------- output
-#                   /
-# x*y _____________/
+# x + y ‾‾‾‾‾‾‾\_________ output
+# x * y _______/
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,19 +10,18 @@ def sigmoid(x):
 def sigmoid_derivative(x):
     return x * (1 - x)
 
-inputs = np.array([[0, 0, 0*0],  # x,y,x*y
-                   [0, 1, 0*1],  
-                   [1, 0, 1*0],  
-                   [1, 1, 1*1]]) 
+inputs = np.array([[0 + 0, 0 * 0],  # x + y, x * y
+                   [0 + 1, 0 * 1],  
+                   [1 + 0, 1 * 0],  
+                   [1 + 1, 1 * 1]]) 
 
-outputs = np.array([[0], [1], [1], [0]])
+outputs = np.array([[0], [1], [1], [0]])  
 
 np.random.seed(42)
-input_layer_neurons = 3  
-output_neurons = 1      
+input_layer_neurons = 2  
+output_neurons = 1       
 
 weights_input_output = np.random.uniform(size=(input_layer_neurons, output_neurons))
-
 bias_output = np.random.uniform(size=(1, output_neurons))
 
 lr = 0.1
@@ -40,21 +36,22 @@ for epoch in range(10000):
     weights_input_output += inputs.T.dot(d_predicted_output) * lr
     bias_output += np.sum(d_predicted_output, axis=0, keepdims=True) * lr
 
-print("Output:")
+print("Output after training:")
 print(predicted_output)
 print("\n----------------------------------")
 
-print("\nW1:")
+print("\nWeights:")
 print(weights_input_output)
 
-print("\nB1:")
+print("\nBias:")
 print(bias_output)
 
 x_min, x_max = 0, 1
 y_min, y_max = 0, 1
 xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
-xy = xx * yy
-grid = np.c_[xx.ravel(), yy.ravel(), xy.ravel()]
+x_plus_y = xx + yy
+x_times_y = xx * yy
+grid = np.c_[x_plus_y.ravel(), x_times_y.ravel()]
 
 output_layer_activation = np.dot(grid, weights_input_output) + bias_output
 grid_predictions = sigmoid(output_layer_activation)
